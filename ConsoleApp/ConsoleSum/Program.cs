122b11176359501ce97a7ffc32c6c0e885c727a7
Zubyk Maksym <ipz224_zmr@student.ztu.edu.ua>
2024-01-27 11:06:02 +0200

    Калькулятор Суми

---
+++ ConsoleApp/ConsoleSum/Program.cs
@@ -0,0 +1,43 @@
+﻿bool continueProgram;
+
+do
+{
+    double num1, num2;
+
+    num1 = ReadValidNumber("Введіть перше число:");
+    num2 = ReadValidNumber("Введіть друге число:");
+
+    double sum = AddNumbers(num1, num2);
+    Console.WriteLine($"Сума чисел {num1} і {num2} дорівнює: {sum}");
+
+    Console.WriteLine("Бажаєте продовжити? (так/ні)");
+    string continueInput = Console.ReadLine();
+    continueProgram = continueInput.ToLower() == "так";
+
+} while (continueProgram);
+
+Console.ReadLine();
+
+static double ReadValidNumber(string prompt)
+{
+    double number;
+    bool isValidInput;
+
+    do
+    {
+        Console.WriteLine(prompt);
+        isValidInput = double.TryParse(Console.ReadLine(), out number);
+        if (!isValidInput)
+        {
+            Console.WriteLine("Неправильний формат числа. Спробуйте ще раз.");
+        }
+    } while (!isValidInput);
+
+    return number;
+}
+
+
+static double AddNumbers(double num1, double num2)
+{
+    return num1 + num2;
+}
